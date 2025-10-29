5933b1d61d48cc6b960a7f51f27b8050dbbc2c40
J Alfonso Martínez <mtz.alfonso96@gmail.com>
2025-10-29 01:45:49 -0600

    refactor: remove distDir configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'dist',
   skipTrailingSlashRedirect: true,
   trailingSlash: true,
   images: {
