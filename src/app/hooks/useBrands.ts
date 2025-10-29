eb99fe2cb4e9c0b9593bbc73b5f73e5cf196b97b
J Alfonso Martínez <mtz.alfonso96@gmail.com>
2025-10-29 00:41:04 -0600

    feat: add useBrands hook to provide a list of brands with logos

---
+++ src/app/hooks/useBrands.ts
@@ -0,0 +1,47 @@
+export const useBrands = () => {
+  const brands = [
+    {
+      name: 'test 1',
+      logoUrl: 'https://static.cdnlogo.com/logos/b/28/bandsports.svg',
+    },
+    {
+      name: 'test 2',
+      logoUrl: 'https://static.cdnlogo.com/logos/t/85/tnt-sports-chile.svg',
+    },
+    {
+      name: 'test 3',
+      logoUrl:
+        'https://static.cdnlogo.com/logos/f/25/federacion-mexicana-de-futbol.svg',
+    },
+    {
+      name: 'test 4',
+      logoUrl: 'https://static.cdnlogo.com/logos/b/28/bandsports.svg',
+    },
+    {
+      name: 'test 5',
+      logoUrl: 'https://static.cdnlogo.com/logos/t/85/tnt-sports-chile.svg',
+    },
+    {
+      name: 'test 6',
+      logoUrl:
+        'https://static.cdnlogo.com/logos/f/25/federacion-mexicana-de-futbol.svg',
+    },
+    {
+      name: 'test 7',
+      logoUrl: 'https://static.cdnlogo.com/logos/b/28/bandsports.svg',
+    },
+    {
+      name: 'test 8',
+      logoUrl: 'https://static.cdnlogo.com/logos/t/85/tnt-sports-chile.svg',
+    },
+    {
+      name: 'test 9',
+      logoUrl:
+        'https://static.cdnlogo.com/logos/f/25/federacion-mexicana-de-futbol.svg',
+    },
+  ];
+
+  return {
+    brands,
+  };
+};
