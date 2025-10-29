a59aa127fb60af8cb5a0198b3166c253597b3bbf
J Alfonso Martínez <olafut10@gmail.com>
2025-10-29 02:18:27 -0600

    refactor: simplify next.config.ts by removing unnecessary configuration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  skipTrailingSlashRedirect: true,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
