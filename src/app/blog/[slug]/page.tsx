b93348df4e03e805eeeb5cdfcee03a60338fd564
J Alfonso Martínez <mtz.alfonso96@gmail.com>
2025-10-29 00:42:27 -0600

    feat: create blog post page with dynamic routing and metadata generation

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,68 @@
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import { PostBody } from '@/app/_components/blog/PostBody';
+import { PostContainer } from '@/app/_components/blog/PostContainer';
+import { PostHeader } from '@/app/_components/blog/PostHeader';
+import { getAllPosts, getPostBySlug } from '@/lib/api';
+import markdownToHtml from '@/lib/markdownToHtml';
+
+type Params = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const post = getPostBySlug(slug);
+
+  if (!post) return notFound();
+
+  const content = await markdownToHtml(post.content || '');
+
+  return (
+    <main>
+      <PostContainer>
+        <PostHeader
+          author={post.author}
+          coverImage={post.coverImage}
+          date={post.date}
+          title={post.title}
+        />
+        <article className="mb-32">
+          <PostBody content={content} />
+        </article>
+      </PostContainer>
+    </main>
+  );
+}
+
+export async function generateStaticParams() {
+  const posts = await getAllPosts();
+
+  return posts.map((post) => ({
+    slug: String(post.slug),
+  }));
+}
+
+export async function generateMetadata(props: Params): Promise<Metadata> {
+  const params = await props.params;
+  const post = getPostBySlug(params.slug);
+
+  if (!post) return notFound();
+
+  const title = `${post.title} | Blog`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+      images: [post.ogImage.url],
+    },
+  };
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,39 @@
+import { getAllPosts } from '@/lib/api';
+import { HeroPost } from '../_components/blog/HeroPost';
+import { MoreStories } from '../_components/blog/MoreStories';
+import { PostContainer } from '../_components/blog/PostContainer';
+import { Title } from '../_components/ui';
+
+export default async function Blog() {
+  const allPosts = await getAllPosts();
+
+  const heroPost = allPosts[0];
+
+  const morePosts = allPosts.slice(1);
+
+  return (
+    <main>
+      <PostContainer>
+        <section className="flex-col md:flex-row flex items-center md:justify-between mt-10 mb-16 md:mb-12">
+          <Title
+            level="h1"
+            className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8"
+          >
+            Blog
+          </Title>
+        </section>
+
+        <HeroPost
+          title={heroPost.title}
+          coverImage={heroPost.coverImage}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </PostContainer>
+    </main>
+  );
+}
