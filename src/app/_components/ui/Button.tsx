a88fb7be826b262042f74dd021d1a490790067ac
olafut <olafut10@gmail.com>
2025-11-09 16:40:48 -0600

    feat: update translations and adjust styles

--- src/app/_components/ui/Button.tsx
+++ src/app/_components/ui/Button.tsx
@@ -6,7 +6,7 @@ export const Button: FC<BaseProps> = ({ children, className, style }) => {
   return (
     <button
       className={twMerge(
-        'px-6 py-4 bg-primary-500 hover:bg-primary-400 text-background rounded-xl text-md font-semibold shadow-lg hover:shadow-xl transition-all duration-300 w-full font-alt flex items-center gap-2 justify-center cursor-pointer uppercase tracking-wider',
+        'px-6 py-4 bg-primary-500 hover:bg-primary-400 text-background rounded-xl text-sm md:text-md font-semibold shadow-lg hover:shadow-xl transition-all duration-300 w-full font-alt flex items-center gap-2 justify-center cursor-pointer uppercase tracking-wider',
         className,
       )}
       style={style}
