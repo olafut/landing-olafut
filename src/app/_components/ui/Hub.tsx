fc4346e77189ae32352699750b76eb6affb5d12e
olafut <olafut10@gmail.com>
2025-11-10 17:07:02 -0600

    fix: update background color in Hub component and enhance Spanish translation for subtitle

--- src/app/_components/ui/Hub.tsx
+++ src/app/_components/ui/Hub.tsx
@@ -4,7 +4,7 @@ import type { BaseProps } from '@/interfaces/base';
 export const Hub: FC<BaseProps> = ({ children }) => {
   return (
     <span className="relative inline-block px-3 py-1 mx-1">
-      <span className="absolute inset-0 bg-primary-100 border-2 border-primary-500 rounded-lg"></span>
+      <span className="absolute inset-0 bg-primary-50 border-2 border-primary-500 rounded-lg"></span>
       <span className="relative font-bold text-primary-500">{children}</span>
     </span>
   );
--- src/messages/es.ts
+++ src/messages/es.ts
@@ -20,7 +20,7 @@ const translations: Translations = {
     title:
       'El <important>Fútbol</important> <important>Femenil</important> solo necesita una comunidad dispuesta <important>a TODO</important>',
     subtitle:
-      'OLAFUT es el HUB del futbol femenil: el corazón que une gradas, canchas, micrófonos y mentes creativas.',
+      '<strong>OLAFUT</strong> es el <tag>HUB</tag> del futbol femenil: el corazón que une gradas, canchas, micrófonos y mentes creativas.',
     trustBadges: {
       activeCommunity: 'Comunidad activa',
       weeklyPodcast: 'Podcast semanal',
